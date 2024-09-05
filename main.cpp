3c66983cc02c99957adff8c22660cfda9d956d63
Seth Rivers <davmrive@ut.utm.edu>
2024-09-04 21:23:56 -0500

    Remade the main.cpp as per assignment's instructions, and edited the git ignore file (changed its paramenters to ignore 'main' instead of 'hello' )

--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 
+int sum(int n){
+  int sum=0; 
+  for (int i = 1; i<=n; i++){
+    sum += i; 
+  }
+  return sum; 
+}
+
 int main() {
-  cout<<"Hello World!"<<endl;
-  
+  int n;
+  cout<<"Please type in a number: ";
+  cin>>n;
+  cout<<"The sum of all the numbers from 1 to "<<n<<" is: "<<sum(n)<<endl; 
   return 0;
 }
