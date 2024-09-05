9ce38482924ea487f79c809704cd7ce651944642
Seth Rivers <davmrive@ut.utm.edu>
2024-09-04 21:47:06 -0500

    Product function added to main and now being pushed into the feature branch

--- main.cpp
+++ main.cpp
@@ -11,10 +11,18 @@ int sum(int n){
   return sum; 
 }
 
+int product(int n){
+  int product=1; 
+  for (int i = 1; i<=n; i++){
+    product *= i; 
+  }
+  return product; 
+}
+
 int main() {
   int n;
   cout<<"Please type in a number: ";
   cin>>n;
-  cout<<"The sum of all the numbers from 1 to "<<n<<" is: "<<sum(n)<<endl; 
+  cout<<"The sum of all the numbers from 1 to "<<n<<" is "<<sum(n)<<", while the product of all the numbers from 1 to "<<n<<" is "<<product(n)<<endl; 
   return 0;
 }
